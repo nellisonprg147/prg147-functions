9057609dfaf48424f5dde47a635d44548a906a68
nellisonprg147 <nellison@students.mchenry.edu>
2016-09-05 01:51:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+function change01(){
+    document.getElementById("big").src="clouds.jpg";
+    document.getElementById("content").innerHTML = "We had about all the types of weather you could think of on this trip.";
+}
+
+function change02(){
+    document.getElementById("big").src="images/rolling_hills.jpg";
+    document.getElementById("content").innerHTML = "Sometimes it felt like the hills would roll up and down for ever. ";
+}
+
+function change03(){
+    document.getElementById("big").src="images/chicken.jpg";
+    document.getElementById("content").innerHTML = " We saw some weird stuff, Like a big chicken.";
+}
+
+function change04(){
+    document.getElementById("big").src="images/bikes.jpg";
+    document.getElementById("content").innerHTML = "On this ride we brought our carbon fiber bikes.";
+}
+
+function change05() {
+    document.getElementById("big").src = "images/debate.jpg";
+    document.getElementById("content").innerHTML = "There was a cool land mark in Freeport memorializing one of presidend Lincoln's debate.";
+}
+function change06(){
+    document.getElementById("big").src="images/downhill.jpg";
+    document.getElementById("content").innerHTML = "Some of the hills outside of Gelena were wicked.";
+}
+
+function change07(){
+    document.getElementById("big").src="images/pavilian.jpg";
+    document.getElementById("content").innerHTML = "Gloomy the first day but the temperatures were nice.";
+}
+function change08(){
+    document.getElementById("big").src="images/sunrise.jpg";
+    document.getElementById("content").innerHTML = "Sunrise on day two of our trip. The morning holds so much optimism.";
+}
+function change09(){
+    document.getElementById("big").src="images/uphill.jpg";
+    document.getElementById("content").innerHTML = "When cycling HILL is a four letter word.";
+}
+function change10(){
+    document.getElementById("big").src="images/windos_desktop.jpg";
+    document.getElementById("content").innerHTML = "I had to stop and get a photo of this, it reminded me of the Bliss Windows 7 default desktop.";
+}
